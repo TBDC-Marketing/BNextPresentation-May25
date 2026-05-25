fd5c21e11cac69a27c4b698aafd767bc1673b676
tbdcmarketing <marketing@tbdc.com>
2026-05-25 11:50:18 -0400

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/BNextPresentation/',
+  base: '/BNextPresentation-May25/',
   publicDir: 'Public',
   plugins: [react()],
-})
+});
